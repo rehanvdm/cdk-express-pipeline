26b662dfed20761d88fec9ee7926919f80fd6c67
rehanvandermerwe <rehan.vdm4@gmail.com>
2025-07-13 17:01:34 +0200

    exclude test

--- test/ci-github.test.ts
+++ test/ci-github.test.ts
@@ -1,8 +1,4 @@
-import * as fsSync from 'fs';
-import * as fs from 'fs/promises';
-import * as path from 'path';
 import { App } from 'aws-cdk-lib';
-import { stringify } from 'yaml';
 import { JsonPatch } from '../src';
 import { CdkExpressPipeline } from '../src/cdk-express-pipeline';
 import { GitHubWorkflowConfig, createGitHubWorkflows, BuildConfig, GithubWorkflow } from '../src/ci-github';
@@ -186,104 +182,104 @@ describe('CDK Express Pipeline CI Configuration', () => {
     }
   });
 
-  it.skip('generate workflows for demo-ts project', async () => {
-
-    const app = new App();
-    const expressPipeline = new CdkExpressPipeline();
-    const wave1 = expressPipeline.addWave('Wave1');
-    const wave1Stage1 = wave1.addStage('Stage1');
-    const stackA = new ExpressStack(app, 'StackA', wave1Stage1);
-    const stackB = new ExpressStack(app, 'StackB', wave1Stage1);
-    stackB.addExpressDependency(stackA);
-    const wave1Stage2 = wave1.addStage('Stage2');
-    new ExpressStack(app, 'Stack2A', wave1Stage2);
-
-    const wave2 = expressPipeline.addWave('Wave2');
-    const wave2Stage1 = wave2.addStage('Stage1');
-    new ExpressStack(app, 'StackC', wave2Stage1);
-
-    const ghConfig: GitHubWorkflowConfig = {
-      synth: {
-        buildConfig: {
-          type: 'preset-npm',
-        },
-        commands: [
-          { dev: "npm run cdk -- synth '**' -c env=dev --output=cdk.out/dev" },
-          { prod: "npm run cdk -- synth '**' -c env=prod --output=cdk.out/prod" },
-        ],
-      },
-      diff: [{
-        id: 'dev',
-        on: {
-          pullRequest: {
-            branches: ['main'],
-          },
-        },
-        stackSelector: 'stage',
-        writeAsComment: true,
-        assumeRoleArn: 'arn:aws:iam::581184285249:role/githuboidc-git-hub-deploy-role',
-        assumeRegion: 'us-east-1',
-        commands: [
-          { dev: 'npm run cdk -- diff {stackSelector} --app=cdk.out/dev' },
-          { prod: 'npm run cdk -- diff {stackSelector} --app=cdk.out/prod' },
-        ],
-      },
-      {
-        id: 'prod',
-        on: {
-          pullRequest: {
-            branches: ['production'],
-          },
-        },
-        stackSelector: 'stage',
-        writeAsComment: true,
-        assumeRoleArn: 'arn:aws:iam::581184285249:role/githuboidc-git-hub-deploy-role',
-        assumeRegion: 'us-east-1',
-        commands: [
-          { prod: 'npm run cdk -- diff {stackSelector} --app=cdk.out/prod' },
-        ],
-      }],
-      deploy: [{
-        id: 'dev',
-        on: {
-          pullRequest: {
-            branches: ['main'],
-          },
-        },
-        stackSelector: 'stack',
-        assumeRoleArn: 'arn:aws:iam::581184285249:role/githuboidc-git-hub-deploy-role',
-        assumeRegion: 'us-east-1',
-        commands: [
-          { dev: 'npm run cdk -- deploy {stackSelector} --app=cdk.out/dev --concurrency 10 --require-approval never --exclusively' },
-        ],
-      },
-      {
-        id: 'prod',
-        on: {
-          push: {
-            branches: ['production'],
-          },
-        },
-        stackSelector: 'stack',
-        assumeRoleArn: 'arn:aws:iam::581184285249:role/githuboidc-git-hub-deploy-role',
-        assumeRegion: 'us-east-1',
-        commands: [
-          { prod: 'npm run cdk -- deploy {stackSelector} --app=cdk.out/prod --concurrency 10 --require-approval never --exclusively' },
-        ],
-      }],
-    };
-
-    const resp = createGitHubWorkflows(ghConfig, expressPipeline.waves);
-    for (const workflow of resp) {
-      const filePath = path.join('/Users/rehanvandermerwe/Rehan/Projects/cdk-express-pipeline-demo-ts/.github', workflow.fileName);
-      const dirPath = path.dirname(filePath);
-      if (!fsSync.existsSync(dirPath)) {
-        fsSync.mkdirSync(dirPath, { recursive: true });
-      }
-
-      await fs.writeFile(filePath, stringify(workflow.content.json));
-    }
-  });
+  // it('generate workflows for demo-ts project', async () => {
+  //
+  //   const app = new App();
+  //   const expressPipeline = new CdkExpressPipeline();
+  //   const wave1 = expressPipeline.addWave('Wave1');
+  //   const wave1Stage1 = wave1.addStage('Stage1');
+  //   const stackA = new ExpressStack(app, 'StackA', wave1Stage1);
+  //   const stackB = new ExpressStack(app, 'StackB', wave1Stage1);
+  //   stackB.addExpressDependency(stackA);
+  //   const wave1Stage2 = wave1.addStage('Stage2');
+  //   new ExpressStack(app, 'Stack2A', wave1Stage2);
+  //
+  //   const wave2 = expressPipeline.addWave('Wave2');
+  //   const wave2Stage1 = wave2.addStage('Stage1');
+  //   new ExpressStack(app, 'StackC', wave2Stage1);
+  //
+  //   const ghConfig: GitHubWorkflowConfig = {
+  //     synth: {
+  //       buildConfig: {
+  //         type: 'preset-npm',
+  //       },
+  //       commands: [
+  //         { dev: "npm run cdk -- synth '**' -c env=dev --output=cdk.out/dev" },
+  //         { prod: "npm run cdk -- synth '**' -c env=prod --output=cdk.out/prod" },
+  //       ],
+  //     },
+  //     diff: [{
+  //       id: 'dev',
+  //       on: {
+  //         pullRequest: {
+  //           branches: ['main'],
+  //         },
+  //       },
+  //       stackSelector: 'stage',
+  //       writeAsComment: true,
+  //       assumeRoleArn: 'arn:aws:iam::581184285249:role/githuboidc-git-hub-deploy-role',
+  //       assumeRegion: 'us-east-1',
+  //       commands: [
+  //         { dev: 'npm run cdk -- diff {stackSelector} --app=cdk.out/dev' },
+  //         { prod: 'npm run cdk -- diff {stackSelector} --app=cdk.out/prod' },
+  //       ],
+  //     },
+  //     {
+  //       id: 'prod',
+  //       on: {
+  //         pullRequest: {
+  //           branches: ['production'],
+  //         },
+  //       },
+  //       stackSelector: 'stage',
+  //       writeAsComment: true,
+  //       assumeRoleArn: 'arn:aws:iam::581184285249:role/githuboidc-git-hub-deploy-role',
+  //       assumeRegion: 'us-east-1',
+  //       commands: [
+  //         { prod: 'npm run cdk -- diff {stackSelector} --app=cdk.out/prod' },
+  //       ],
+  //     }],
+  //     deploy: [{
+  //       id: 'dev',
+  //       on: {
+  //         pullRequest: {
+  //           branches: ['main'],
+  //         },
+  //       },
+  //       stackSelector: 'stack',
+  //       assumeRoleArn: 'arn:aws:iam::581184285249:role/githuboidc-git-hub-deploy-role',
+  //       assumeRegion: 'us-east-1',
+  //       commands: [
+  //         { dev: 'npm run cdk -- deploy {stackSelector} --app=cdk.out/dev --concurrency 10 --require-approval never --exclusively' },
+  //       ],
+  //     },
+  //     {
+  //       id: 'prod',
+  //       on: {
+  //         push: {
+  //           branches: ['production'],
+  //         },
+  //       },
+  //       stackSelector: 'stack',
+  //       assumeRoleArn: 'arn:aws:iam::581184285249:role/githuboidc-git-hub-deploy-role',
+  //       assumeRegion: 'us-east-1',
+  //       commands: [
+  //         { prod: 'npm run cdk -- deploy {stackSelector} --app=cdk.out/prod --concurrency 10 --require-approval never --exclusively' },
+  //       ],
+  //     }],
+  //   };
+  //
+  //   const resp = createGitHubWorkflows(ghConfig, expressPipeline.waves);
+  //   for (const workflow of resp) {
+  //     const filePath = path.join('/Users/rehanvandermerwe/Rehan/Projects/cdk-express-pipeline-demo-ts/.github', workflow.fileName);
+  //     const dirPath = path.dirname(filePath);
+  //     if (!fsSync.existsSync(dirPath)) {
+  //       fsSync.mkdirSync(dirPath, { recursive: true });
+  //     }
+  //
+  //     await fs.writeFile(filePath, stringify(workflow.content.json));
+  //   }
+  // });
 });
 
 describe('JsonPatch and GithubWorkflow.patch()', () => {
